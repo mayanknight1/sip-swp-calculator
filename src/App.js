85c5f03014b84749e9a76857f9ddce1ed51cf56c
mayanknight1 <idabsent@gmail.com>
2025-04-19 20:02:07 +0530

    dark theme alignment and bg update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Calculator from './components/Calculator';
 import { useTheme } from './ThemeContext';
 
 function App() {
-    const { isDarkMode } = useTheme();
+  const { isDarkMode } = useTheme();
 
-    return (
-        <div className={isDarkMode ? 'dark-mode' : ''}>
-            <Calculator />
-        </div>
-    );
+  useEffect(() => {
+    if (isDarkMode) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  }, [isDarkMode]);
+
+  return (
+    <div className={isDarkMode ? 'dark-mode' : ''}>
+      <Calculator />
+    </div>
+  );
 }
 
 export default App;
--- src/components/ToggleSlider.js
+++ src/components/ToggleSlider.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { useTheme } from '../ThemeContext';
-import './ToggleSlider.css'; // Import the CSS file
+import './ToggleSlider.css';
 
 const ToggleSlider = () => {
   const { isDarkMode, toggleTheme } = useTheme();
 
   return (
-    <label className="toggle-switch">
-      <input type="checkbox" checked={isDarkMode} onChange={toggleTheme} />
-      <span className="slider round">
-        <span className="light-mode">Light</span>
-        <span className="dark-mode">Dark</span>
-      </span>
-    </label>
+    <div className="theme-toggle-wrapper">
+      <span className="theme-label">Light</span>
+      <label className="toggle-switch">
+        <input 
+          type="checkbox" 
+          checked={isDarkMode} 
+          onChange={toggleTheme}
+        />
+        <span className="slider round"></span>
+      </label>
+      <span className="theme-label">Dark</span>
+    </div>
   );
 };
 

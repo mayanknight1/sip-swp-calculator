5120fbdf67e6a173f8aac6b9123756946105cc1a
mayanknight1 <idabsent@gmail.com>
2025-04-19 13:14:59 +0530

    SIP and SWP calc written removed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import Calculator from './components/Calculator';
-import './styles/styles.css';
 
 function App() {
   return (
-    <div className="calculator-container">
-      <div className="calculator-card">
-        <h1 className="title">SIP and SWP Calculator</h1>
-        <Calculator />
-      </div>
+    <div>
+      <Calculator />
     </div>
   );
 }
